4d6ea2d893835947a41133a4ac9ad2a47adaf0b9
Oliver Levy <oliver.levy.ol@gmail.com>
2020-08-03 14:22:26 -0400

    working on user sign in info to be stored in redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
 import React from "react";
 import "./App.scss";
 import { useSelector, useDispatch } from "react-redux";
-import { increment, decrement } from "./actions";
-import Login from "./components/Login";
+import { increment, decrement, login } from "./actions";
+// import { firebaseConfig } from "./config";
+// import * as firebase from "firebase/app";
+// import "firebase/auth";
 
-function App() {
+// firebase.initializeApp(firebaseConfig);
+
+// var provider = new firebase.auth.GoogleAuthProvider();
+
+// function SignIn() {
+//   firebase
+//     .auth()
+//     .signInWithPopup(provider)
+//     .then(function (result) {
+//       // This gives you a Google Access Token. You can use it to access the Google API.
+//       var token = result.credential.accessToken;
+//       // The signed-in user info.
+//       var user = result.user;
+
+//       console.log(token);
+//       console.log(user);
+//       // ...
+//     })
+//     .catch(function (error) {
+//       // Handle Errors here.
+//       var errorCode = error.code;
+//       var errorMessage = error.message;
+//       // The email of the user's account used.
+//       var email = error.email;
+//       // The firebase.auth.AuthCredential type that was used.
+//       var credential = error.credential;
+//       // ...
+//     });
+// }
+
+export function App() {
   const counter = useSelector((state) => state.counter);
+  const login = useSelector((state) => state.login);
   const dispatch = useDispatch();
   return (
     <div className="App">
@@ -13,8 +46,6 @@ function App() {
       <h2>counter {counter}</h2>
       <button onClick={() => dispatch(increment(2))}>+</button>
       <button onClick={() => dispatch(decrement())}>-</button>
-
-      <Login/>
     </div>
   );
 }
