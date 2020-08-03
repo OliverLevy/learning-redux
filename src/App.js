d7e49f9a65253a378829b23d9f3c541a9c9ee407
Oliver Levy <oliver.levy.ol@gmail.com>
2020-08-03 12:32:40 -0400

    adding google auth

--- src/App.js
+++ src/App.js
@@ -1,6 +1,37 @@
 import React from "react";
+import "./App.scss";
 import { useSelector, useDispatch } from "react-redux";
 import { increment, decrement } from "./actions";
+import { firebaseConfig } from "./config";
+import * as firebase from "firebase/app";
+import "firebase/auth";
+
+firebase.initializeApp(firebaseConfig);
+
+var provider = new firebase.auth.GoogleAuthProvider();
+
+const signIn = () => {
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then(function (result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      // ...
+    })
+    .catch(function (error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+};
 
 function App() {
   const counter = useSelector((state) => state.counter);
@@ -12,6 +43,8 @@ function App() {
       <h2>counter {counter}</h2>
       <button onClick={() => dispatch(increment(2))}>+</button>
       <button onClick={() => dispatch(decrement())}>-</button>
+
+      <button onClick={() => signIn()}>Login</button>
       {isLogged ? <p>users info</p> : ""}
     </div>
   );
---
+++ src/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBE6nFApMCynB14RI8kRz5SAkGdSyogaFE",
+  authDomain: "redux-auth-dfe16.firebaseapp.com",
+  databaseURL: "https://redux-auth-dfe16.firebaseio.com",
+  projectId: "redux-auth-dfe16",
+  storageBucket: "redux-auth-dfe16.appspot.com",
+  messagingSenderId: "225612861804",
+  appId: "1:225612861804:web:cf8d8e77b9d94a73d3a349",
+  measurementId: "G-H6ZE4RYGVZ",
+};
